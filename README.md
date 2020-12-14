12fc68ac1a76e66bc81406a057a2ddd7a8e11a6d
Yuuhe Nagisa <nagisayuuhe@gmail.com>
2020-12-14 21:43:15 +0900

    プロトタイプ完成

---
+++ README.md
@@ -0,0 +1,13 @@
+# K平均方の動作を確認するサンプル
+
+※参考サイト
+https://webbibouroku.com/Blog/Article/sklearn-cluster-flag
+
+## 実行環境
+$ python --version
+Python 3.8.6
+
+## 実行方法
+$ ./exec_clustering.py
+
+
--- exec_clustering.py
+++ exec_clustering.py
@@ -2,7 +2,7 @@ import os
 import shutil
 import numpy as np
 from PIL import Image
-from skimage import data
+from skimage import io
 from sklearn.cluster import KMeans
 
 # 1. 国旗画像のサイズをそろえて保存する
@@ -14,3 +14,19 @@ for path in os.listdir('./flag_origin'):
     img_resize = img.resize((200, 100))
     img_resize.save(f'./flag_convert/{path}.jpg')
 
+# 2. 3次元配列の画像データを2次元配列のデータに変換
+feature = np.array([io.imread(f'./flag_convert/{path}',plugin='matplotlib') for path in os.listdir('./flag_convert')])
+feature = feature.reshape(len(feature), -1).astype(np.float64)
+
+# 3. 学習(15種類のグループにクラスタリングする)
+model = KMeans(n_clusters=15).fit(feature)
+
+# 4. 学習結果のラベル
+labels = model.labels_
+
+# 5. 学習結果(クラスタリング結果の表示 + ラベルごとにフォルダ分け)
+# ./flag_group 以下に画像を分けて保存する
+for label, path in zip(labels, os.listdir('./flag_convert')):
+    os.makedirs(f"./flag_group/{label}", exist_ok=True)
+    shutil.copyfile(f"./flag_origin/{path.replace('.jpg', '')}", f"./flag_group/{label}/{path.replace('.jpg', '')}")
+    print(label, path)
